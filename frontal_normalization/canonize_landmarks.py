667d556c711d22492106963663720bb5d759688a
AbrehamT <abrehamtadesse95@gmail.com>
2025-04-26 16:57:44 -0700

    added cannonical points

--- frontal_normalization/canonize_landmarks.py
+++ frontal_normalization/canonize_landmarks.py
@@ -9,5 +9,11 @@ class Canonize():
         # Defining key points on the human face into a canonical pose
         # nose will be at origin and eyes are ~60 units apart from each other
         # Pupil's are about ~ 30 units away from the nose tip 
+        self.model_points = np.array([
+            (0.0, 0.0, 0.0), #nose
+            (-30.0, 30.0, -10.0), #left eye outer corner
+            (30.0, 30.0, -10.0) #right eye outer corner
+        ])
 
-        
+    def mediapipe_landmarks():
+        pass
